7949cb217b44e409153bff632c3d8bdeb9ee1107
Pawel <pawel@pawel.com>
2022-11-27 16:09:43 +0100

    microservices

---
+++ src/main/java/com/example/discovery/DiscoveryApplication.java
@@ -0,0 +1,17 @@
+package com.example.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+//Rejestr usług, to jest server musi miec eurekaserver w pomie
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
+
+}
